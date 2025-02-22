03618a82d38312296dc89de7010326b211f1e8e5
Krish Sahu <krishsah5216@gmail.com>
2025-02-22 16:07:19 +0530

    commit

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def detect_license_plate():
     return jsonify({"plates": plates})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
